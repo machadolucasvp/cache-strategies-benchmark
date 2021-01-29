5f968fc658af3d6ff822931813ebd0275fab6c3a
Lucas Machado <machadolucasvp@gmail.com>
2021-01-29 00:15:33 -0300

    refactor: add logging for trace request and cache miss info

--- packages/redis-server/src/api/controllers/message-controller.js
+++ packages/redis-server/src/api/controllers/message-controller.js
@@ -1,14 +1,19 @@
 const MessageService = require('../services/message-service')
+const Logger = require('../../config/logger')
+
+const logger = Logger.getLogger('message-controller')
 
 class MessageController {
     static async onGet(req, res) {
         const { id } = req.params
+        logger.trace(`receiving request for ${id}`)
 
         res.send(await MessageService.getMessage(id))
     }
 
     static async onGetByDescription(req, res) {
         const { searchDescription } = req.params
+        logger.trace(`receiving request for ${searchDescription}`)
 
         res.send(await MessageService.searchByDescription(searchDescription))
     }
--- packages/redis-server/src/api/services/message-service.js
+++ packages/redis-server/src/api/services/message-service.js
@@ -1,13 +1,16 @@
 const CacheService = require('../services/cache-service')
 const Message = require("../models/message-model")
+const Logger = require('../../config/logger')
 
 const Cache = CacheService.getContext('message')
+const logger = Logger.getLogger('message-service')
 
 class MessageService {
     static async getMessage(id) {
         const cachedMesage = await Cache.recover(id)
 
         if (!cachedMesage) {
+            logger.info(`cache miss message:${id}`)
             const message = await Message.findById(id)
 
             return Cache.set(id, message)
@@ -19,6 +22,7 @@ class MessageService {
         const cachedMessage = await Cache.recover(description)
 
         if (!cachedMessage) {
+            logger.info(`cache miss message:${description}`)
             const message = Message.find({ description: { $regex: description } })
 
             return Cache.set(description, message)

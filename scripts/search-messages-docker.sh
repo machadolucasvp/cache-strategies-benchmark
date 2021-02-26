49e9572a78e1e19e946b29a5a7e28a14c76a0fc0
Lucas Machado <machadolucasvp@gmail.com>
2021-02-25 23:47:08 -0300

    fix: resolve wrong description property in message seed data

--- scripts/search-messages-docker.sh
+++ scripts/search-messages-docker.sh
@@ -2,4 +2,4 @@ echo \
 "$(docker inspect $1 -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}')":\
 "$(docker inspect $1 -f '{{ .Config.Env }}' | tr ' ' '\n' | grep API_PORT | sed 's/^.*=//')"\
 | xargs -I '$:dns' echo 'http://$:dns/messages' \
-| xargs -I '$:target' artillery run ./searchMessages.yml --target '$:target'
+| xargs -I '$:target' artillery run ./scripts/searchMessages.yml --target '$:target'
